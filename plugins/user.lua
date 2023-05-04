0be146be9bcf385aac96d8808e0639d1ddd317e5
nicolas manay <nico281@hotmail.com>
2023-05-04 14:34:04 -0300

    git ui lib

--- plugins/user.lua
+++ plugins/user.lua
@@ -21,6 +21,7 @@ return {
           -- require('hover.providers.man')
           -- require('hover.providers.dictionary')
         end,
+
         preview_opts = {
           border = nil
         },
@@ -34,7 +35,43 @@ return {
       vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
       vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
     end
-  }
+  },
+  {
+    "aspeddro/gitui.nvim",
+    config = function()
+      require("gitui").setup {
+        {
+          -- Command Options
+          command = {
+            -- Enable :Gitui command
+            -- @type: bool
+            enable = true,
+          },
+          -- Path to binary
+          -- @type: string
+          binary = "gitui",
+          -- Argumens to gitui
+          -- @type: table of string
+          args = {},
+          -- WIndow Options
+          window = {
+            options = {
+              -- Width window in %
+              -- @type: number
+              width = 90,
+              -- Height window in %
+              -- @type: number
+              height = 80,
+              -- Border Style
+              -- Enum: "none", "single", "rounded", "solid" or "shadow"
+              -- @type: string
+              border = "rounded",
+            },
+          },
+        }
+      }
+    end
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
